8aeefc9a8aa191051e67c8e83165051c934595b5
astha0107 <goswamiastha0107@gmail.com>
2026-02-04 14:12:23 +0530

    commit

--- index.js
+++ index.js
@@ -159,3 +159,13 @@ server.listen(3000, () => {
 
 
 
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World from Node HTTP Server");
+});
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
